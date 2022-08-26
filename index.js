125567f0de3e817f552e5445e5c96ad8c0bd67c8
Ali Fasihy <fasihy.ali@gmail.com>
2022-08-26 11:16:24 -0400

    edit 1

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const express = require('express');
  
  // Define a "root" route directly on app
  // Tomorrow, we'll use best practice routing
- app.get('/', function (req, res) {
+ app.get('myfirstcookingapp.herokuapp.com request_id=bbc6106c-adf8-4ff7-bbfc-bb100426234a fwd="99.231.250.37"', function (req, res) {
    res.send('<h1>First Cooking app!</h1>');
  });
