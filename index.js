0ca3547a51658ad5f24de0300e78d549d4928605
Ali Fasihy <fasihy.ali@gmail.com>
2022-08-26 14:43:51 -0400

    edit 2

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+// Load express
 const express = require('express');
  
- // Create our express app
- const app = express();
- 
- // Define a "root" route directly on app
- // Tomorrow, we'll use best practice routing
- app.get('myfirstcookingapp.herokuapp.com request_id=bbc6106c-adf8-4ff7-bbfc-bb100426234a fwd="99.231.250.37"', function (req, res) {
-   res.send('<h1>First Cooking app!</h1>');
- });
+// Create our express app
+const app = express();
+const port = process.env.port || 3000
+
+// Define a "root" route directly on app
+// Tomorrow, we'll use best practice routing
+app.get('/', function (req, res) {
+  res.send('<h1>First Cooking Application !</h1>');
+});
+
+// Tell the app to listen on port 3000
+// for HTTP requests from clients
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
