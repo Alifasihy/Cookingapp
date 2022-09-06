2df5cbadb72ac7e53a5fc12e493d6e87a5d2ca1a
Ali Fasihy <fasihy.ali@gmail.com>
2022-09-06 18:01:20 -0400

    add partials

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Load express
-const express = require('express');
- 
-// Create our express app
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+const appName= process.env.expressAppName || 'Cooking Match'
 
-// Define a "root" route directly on app
-// Tomorrow, we'll use best practice routing
-app.get('/', function (req, res) {
-  res.send('<h1>First Cooking Application !</h1>');
-});
+app.set('views', './views')
+app.set('view-engine', 'ejs')
 
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`${appName} app is listening on port ${port}`)
+})
+
